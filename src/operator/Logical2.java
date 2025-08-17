944500504c90b6b7ce4b27fa6d429b304cb8254a
이경준 <kyoungjun7777@naver.com>
2025-08-17 17:09:41 +0900

    논리 연산자

---
+++ src/operator/Logical2.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Logical2 {
+    public static void main(String[] args) {
+
+    boolean stop = true;
+    if(stop){
+        System.out.println("중지합니다.");
+    }
+    }
+}
