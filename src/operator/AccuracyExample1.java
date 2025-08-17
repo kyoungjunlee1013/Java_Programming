5b37027a2ad0b0375719e010ffc2b1f1decc2d6d
이경준 <kyoungjun7777@naver.com>
2025-08-17 19:25:22 +0900

    AccuracyExample

---
+++ src/operator/AccuracyExample1.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class AccuracyExample1 {
+    public static void main(String[] args) {
+        int apple = 1;
+        double pieceUnit = 0.1;
+        int number = 7;
+
+        double result = apple - number*pieceUnit;
+        System.out.println("사과 1개에 남은 양 " + result);
+    }
+}
