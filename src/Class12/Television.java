ca675400b318065bd2d8e57b8ea06261efbf0f53
이경준 <kyoungjun7777@naver.com>
2025-08-18 21:26:22 +0900

    Class12

---
+++ src/Class12/Television.java
@@ -0,0 +1,11 @@
+package Class12;
+
+public class Television {
+    static String company = "MyCompany";
+    static String model = "LCD";
+    static String info;
+
+    static{
+        info = company + "-" + model;
+    }
+}
