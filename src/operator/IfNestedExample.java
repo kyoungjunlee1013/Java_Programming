d3f10e5ad2dc8ce8322cbe308eb9b1263e0e01bd
이경준 <kyoungjun7777@naver.com>
2025-08-18 13:48:28 +0900

    IfNestedExample

---
+++ src/operator/IfNestedExample.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class IfNestedExample {
+    public static void main(String[] args) {
+        int score = (int)(Math.random()*20) + 81;
+        System.out.println("점수: " + score);
+
+        String grade;
+
+        if(score >= 90) {
+            grade = "A+";
+        } else{
+            grade = "B+";
+        }
+        System.out.println("학점: " + grade);
+    }
+}
