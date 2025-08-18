250bd98c24fa47a0fe274b1a1f9250aef36f3137
이경준 <kyoungjun7777@naver.com>
2025-08-18 21:34:35 +0900

    Class15

---
+++ src/Class14/KoreanExample.java
@@ -0,0 +1,15 @@
+package Class14;
+
+public class KoreanExample {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("123456-1234567","김자바");
+
+        System.out.println(k1.nation);
+        System.out.println(k1.ssn);
+        System.out.println(k1.name);
+
+
+        k1.name = "자바";
+        System.out.println(k1.name);
+    }
+}
