e32a1fe0bea855f4756488c87af8ea9b8756a1bd
이경준 <kyoungjun7777@naver.com>
2025-08-18 16:12:56 +0900

    EmptyStringExample

---
+++ src/ReferenceType/EmptyStringExample.java
@@ -0,0 +1,10 @@
+package ReferenceType;
+
+public class EmptyStringExample {
+    public static void main(String[] args) {
+        String hobby = "";
+        if(hobby.equals("")){
+            System.out.println("hooby 변수가 참조하는 String 객체는 빈 문자열");
+        }
+    }
+}
