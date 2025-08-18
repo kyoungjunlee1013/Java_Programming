905f00a10f8355e34ec520407f68d85c3db7c5f1
이경준 <kyoungjun7777@naver.com>
2025-08-18 13:37:27 +0900

    IfElseExample

--- src/operator/IfExample.java
+++ src/operator/IfExample.java
@@ -8,9 +8,9 @@ public class IfExample {
             System.out.println("점수가 90점 이상입니다.");
             System.out.println("등급은 A입니다.");
         }
-        if(score < 90){
+        if(score < 90)
             System.out.println("점수가 90 미만입니다.");
             System.out.println("등급은 B입니다.");
-        }
+
     }
 }
