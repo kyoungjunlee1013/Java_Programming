abe97dc6aa92c02e60b11bde278b801e290b01de
이경준 <kyoungjun7777@naver.com>
2025-08-18 14:21:00 +0900

    SwitchValueExample

---
+++ src/operator/PrintFFrom1To10Example.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class PrintFFrom1To10Example {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.print(i+ " ");
+        }
+    }
+}
