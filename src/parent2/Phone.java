db9a04c7049895fd33142fc624ff719ab5878df0
이경준 <kyoungjun7777@naver.com>
2025-08-19 14:09:26 +0900

    parent2

---
+++ src/parent2/Phone.java
@@ -0,0 +1,11 @@
+package parent2;
+
+public class Phone {
+    
+    public String model;
+    public String color;
+    
+    public Phone(){
+        System.out.println("phone() 생성자 실행");
+    }
+}
