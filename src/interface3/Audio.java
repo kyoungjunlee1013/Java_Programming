fbd66a8fa4926e7beaf703af98c9313d3a305e53
이경준 <kyoungjun7777@naver.com>
2025-08-19 18:27:25 +0900

    interface3

---
+++ src/interface3/Audio.java
@@ -0,0 +1,25 @@
+package interface3;
+
+public class Audio implements RemoteControl{
+    private int volume;
+
+    @Override
+    public void turnOn(){
+        System.out.println("Audio is turned on");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Audio is turned off");
+    }
+    @Override
+    public void setVolume(int volume) {
+        if(volume>RemoteControl.MAX_VOLUME){
+            this.volume = RemoteControl.MAX_VOLUME;
+        }else if(volume<RemoteControl.MIN_VOLUME){
+            this.volume = RemoteControl.MIN_VOLUME;
+        }else{
+            this.volume = volume;
+        }
+        System.out.println("현재 Audio 볼륨: " + this.volume);
+    }
+}
