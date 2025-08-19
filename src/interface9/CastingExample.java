0d62fc3183c946a8a3e6f0a59b52777803d9a384
이경준 <kyoungjun7777@naver.com>
2025-08-19 19:58:29 +0900

    interface9

---
+++ src/interface9/CastingExample.java
@@ -0,0 +1,13 @@
+package interface9;
+
+public class CastingExample {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Bus();
+
+        vehicle.run();
+
+        Bus bus = (Bus) vehicle;
+        bus.run();
+        bus.checkFare();
+    }
+}
---
+++ src/interface9/Vehicle.java
@@ -0,0 +1,5 @@
+package interface9;
+
+public interface Vehicle {
+    void run();
+}
