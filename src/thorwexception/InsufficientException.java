68b7bbb652edd8efe74500f7fb52e194e8dd5e76
이경준 <kyoungjun7777@naver.com>
2025-08-20 18:45:24 +0900

    InsufficientException

---
+++ src/thorwexception/InsufficientException.java
@@ -0,0 +1,10 @@
+package thorwexception;
+
+public class InsufficientException extends Exception {
+    public InsufficientException() {
+
+    }
+    public InsufficientException(String message) {
+        super(message);
+    }
+}
