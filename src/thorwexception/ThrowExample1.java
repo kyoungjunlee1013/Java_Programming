2b44824644ec7336289f2516865f454062422fd5
이경준 <kyoungjun7777@naver.com>
2025-08-20 18:35:28 +0900

    ThrowExample1

---
+++ src/thorwexception/ThrowExample1.java
@@ -0,0 +1,14 @@
+package thorwexception;
+
+public class ThrowExample1 {
+    public static void main(String[] args) {
+        try {
+            findclass();
+        } catch (ClassNotFoundException e){
+            System.out.println("예외 처리: " + e.toString());
+        }
+    }
+    public static void findclass() throws ClassNotFoundException{
+        Class.forName("java.lang.String2");
+    }
+}
