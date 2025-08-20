f704f948ad0e2107e25383a9be56c49301f0415d
이경준 <kyoungjun7777@naver.com>
2025-08-20 18:10:10 +0900

    ExceptionHandLingExample

---
+++ src/exception6/ExceptionHandLingExample.java
@@ -0,0 +1,19 @@
+package exception6;
+
+public class ExceptionHandLingExample {
+    public static void main(String[] args) {
+        String[] array = {"100", "1oo", null, "200"};
+
+        for(int i=0;i<array.length;i++){
+            try {
+                int value = Integer.parseInt(array[i]);
+                System.out.println("array[" + i + "] = " + value);
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("배열 인덱스 초과됨" + e.getMessage());
+            }catch (NullPointerException | NumberFormatException e){
+                System.out.println("데이터에 문제 있음" + e.getMessage());
+            }
+        }
+    }
+}
+
