31712ca2ceb2b80ffcc739f53a1010ae0255ab49
이경준 <kyoungjun7777@naver.com>
2025-08-20 16:12:46 +0900

    nestedinterface1,2,3

---
+++ src/nestedinterface1/Button.java
@@ -0,0 +1,7 @@
+package nestedinterface1;
+
+public class Button {
+    public static interface ClickListener{
+        void onClick();
+    }
+}
---
+++ src/nestedinterface2/Button.java
@@ -0,0 +1,12 @@
+package nestedinterface2;
+
+public class Button {
+    public static interface ClickListener{
+        void onClick();
+    }
+        private ClickListener clickListener;
+
+        public void setClickListener(ClickListener clickListener){
+            this.clickListener = clickListener;
+        }
+}
