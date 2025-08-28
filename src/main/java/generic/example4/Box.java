8833d802cf42b1cc4758a3a86af3ece2250cc374
이경준 <kyoungjun7777@naver.com>
2025-08-28 17:48:03 +0900

    GenericExample

---
+++ src/main/java/generic/example4/Box.java
@@ -0,0 +1,10 @@
+package generic.example4;
+
+public class Box<T> {
+    public T content;
+
+    public boolean compare(Box<T> other){
+        boolean result = content.equals(other.content);
+        return result;
+    }
+}
