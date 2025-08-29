ffcba7a2ee8ccf5d7d59e48526e133b9e8f74fd0
이경준 <kyoungjun7777@naver.com>
2025-08-29 17:40:16 +0900

    HashSetExample

---
+++ src/main/java/collection/example4/HashSetExample.java
@@ -0,0 +1,19 @@
+package collection.example4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+        set.add("java");
+        set.add("jdbc");
+        set.add("jsp");
+        set.add("java");
+        set.add("spring");
+
+        int size = set.size();
+        System.out.println("총 객체 수: " + size);
+    }
+}
