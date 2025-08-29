e6e95da80b16849a782b87d47a30aa19ba70448e
이경준 <kyoungjun7777@naver.com>
2025-08-29 14:37:39 +0900

    SafeStopExample

---
+++ src/main/java/thread/example10/PrintThread.java
@@ -0,0 +1,17 @@
+package thread.example10;
+
+public class PrintThread extends Thread{
+    private boolean stop;
+    
+    public void setStop(boolean stop){
+        this.stop = stop;
+    }
+    @Override
+    public void run(){
+        while(!stop){
+            System.out.println("실행중");
+        }
+        System.out.println("리소스 정리");
+        System.out.println("실행 종료");
+    }
+}
