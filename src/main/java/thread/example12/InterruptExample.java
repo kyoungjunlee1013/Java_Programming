afe6a8880937760ccacb0332d75e571672c885fa
이경준 <kyoungjun7777@naver.com>
2025-08-29 14:54:05 +0900

    InterruptExample

---
+++ src/main/java/thread/example12/InterruptExample.java
@@ -0,0 +1,16 @@
+package thread.example12;
+
+import thread.example11.PrintThread;
+
+public class InterruptExample {
+    public static void main(String[] args) {
+        Thread thread = new PrintThread();
+        thread.start();
+
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){}
+
+        thread.interrupt();
+    }
+}
