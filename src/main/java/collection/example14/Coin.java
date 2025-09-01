47ffb0c7bf4f1495faecb681aa7bbcb710a14da0
이경준 <kyoungjun7777@naver.com>
2025-09-01 18:19:02 +0900

    StackExample

---
+++ src/main/java/collection/example14/Coin.java
@@ -0,0 +1,12 @@
+package collection.example14;
+
+public class Coin {
+    private int value;
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
