3ae8481dbefc9ae02126d9099f80f56417533ab6
이경준 <kyoungjun7777@naver.com>
2025-09-01 19:05:14 +0900

    ImmutableExample

--- src/main/java/collection/example16/SynchronizedMapExample.java
+++ src/main/java/collection/example16/SynchronizedMapExample.java
@@ -11,7 +11,7 @@ public class SynchronizedMapExample {
         Thread threadA = new Thread(){
             @Override
             public void run() {
-                for(int i=0; i<=1000; i++){
+                for(int i=1; i<=1000; i++){
                     map.put(i, "내용"+i);
                 }
             }
