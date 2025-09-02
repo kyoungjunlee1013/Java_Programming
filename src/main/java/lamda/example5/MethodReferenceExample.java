69cd7907f0849282fb10c95dcf7917fde712b5e1
이경준 <kyoungjun7777@naver.com>
2025-09-02 18:33:30 +0900

    public class LamdaExample {

---
+++ src/main/java/lamda/example5/MethodReferenceExample.java
@@ -0,0 +1,13 @@
+package lamda.example5;
+
+public class MethodReferenceExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.action(Computer :: staticMethod);
+
+        Computer com = new Computer();
+
+        person.action(com :: instanceMethod);
+    }
+}
