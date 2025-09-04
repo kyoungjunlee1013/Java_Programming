e6334df341639fb887bf96cb30754772fd15d38d
이경준 <kyoungjun7777@naver.com>
2025-09-04 16:39:05 +0900

    ReadExample

--- src/main/java/datastream/example3/WriteExample.java
+++ src/main/java/datastream/example3/WriteExample.java
@@ -7,7 +7,7 @@ import java.io.OutputStream;
 public class WriteExample {
     public static void main(String[] args) {
         try {
-            OutputStream os = new FileOutputStream("C:/tem/Temp/test3.db");
+            OutputStream os = new FileOutputStream("C:/Temp/test3.db");
 
             byte[] array = {10, 20, 30, 40, 50};
 
